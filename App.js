fc4f82773287c63a828a92a7678a654e46f99a56
Anjusree16 <144591523+Anjusree16@users.noreply.github.com>
2023-12-09 19:59:18 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+      <SignupForm />
+    </div>
+  );
+};
+
+export default App;
